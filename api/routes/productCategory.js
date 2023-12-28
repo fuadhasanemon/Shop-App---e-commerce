a42b243fe74869a6c6f3c4eafba9f449b5f419da
Fuad Emon <fuad.emon@springrain.io>
2023-12-29 00:36:06 +0600

    Category CRUD Done

--- api/routes/productCategory.js
+++ api/routes/productCategory.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { getAllProductCategory } from "../controller/productCategoryController.js";
+import {
+  getAllProductCategory,
+  createProductCategory,
+  getSingleProductCategory,
+  deleteProductCategory,
+  updateProductCategory
+} from "../controller/productCategoryController.js";
+import { productCategoryMulter } from "../utils/multer.js";
 
 // create router
 const router = express.Router();
 
 // routes
 router.get("/category", getAllProductCategory);
+router.post("/category", productCategoryMulter, createProductCategory);
+router.get("/category/:slug", getSingleProductCategory);
+router.delete("/category/:id", deleteProductCategory);
+router.put("/category/:id", updateProductCategory);
+router.patch("/category/:id", updateProductCategory);
 
 export default router;
