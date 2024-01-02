575e1085acc7fb73f42431f20f412c853e126b94
Fuad Emon <fuad.emon@springrain.io>
2024-01-02 08:46:56 +0600

    Brand apis cooking

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ import colors from "colors";
 import dotenv from "dotenv";
 import cors from "cors";
 import productCategoryRoute from "./routes/productCategory.js";
+import productBrandRoute from "./routes/productBrand.js";
 import mongodbConnect from "./config/db.js";
+import { errorHandle } from "./middlewares/errorHandler.js";
 
 // init express
 const app = express();
@@ -19,9 +21,13 @@ app.use(express.static("api/public"));
 
 // routes
 app.use("/api/v1/product", productCategoryRoute);
+app.use("/api/v1/product", productBrandRoute);
 
 // Environvent variable
 const PORT = process.env.PORT || 9090;
+
+// Error handller
+app.use(errorHandle);
 
 // listen server
 app.listen(PORT, () => {
